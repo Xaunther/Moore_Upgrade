175b4b519664a4ed4401a16c632ecd69fcfb753f
aalfonso <alejandro.alfonso.albero@cern.ch>
2020-06-02 15:29:00 +0200

    Add LambdaG and PhiG DSTs

---
+++ Moore_Scripts/LambdaG.py
@@ -0,0 +1,27 @@
+from Moore import options
+from Gaudi.Configuration import INFO
+import os
+
+#Define outputfolder here
+outfolder = "output/LambdaG/"
+try:
+    os.mkdir(outfolder)
+except:
+    pass
+
+with open("DSTs/LambdaG_15102307_MagDown.dir") as f:
+    options.input_files = f.read().splitlines()
+f.close()
+options.input_type = 'ROOT'
+options.input_raw_format = 4.3
+options.evt_max = -1
+options.print_freq = 10000
+options.data_type = 'Upgrade'
+options.dddb_tag = 'dddb-20171126'
+options.conddb_tag = 'sim-20171127-vc-md100'
+options.simulation = True
+options.output_file = outfolder+'LambdaG.mdst'
+options.output_type = 'ROOT'
+options.output_level = INFO
+options.control_flow_file = outfolder+'control_flow.gv'
+options.data_flow_file = outfolder+'data_flow.gv'
---
+++ Moore_Scripts/PhiG.py
@@ -0,0 +1,27 @@
+from Moore import options
+from Gaudi.Configuration import INFO
+import os
+
+#Define outputfolder here
+outfolder = "output/PhiG/"
+try:
+    os.mkdir(outfolder)
+except:
+    pass
+
+with open("DSTs/PhiG_13102202_MagDown.dir") as f:
+    options.input_files = f.read().splitlines()
+f.close()
+options.input_type = 'ROOT'
+options.input_raw_format = 4.3
+options.evt_max = -1
+options.print_freq = 10000
+options.data_type = 'Upgrade'
+options.dddb_tag = 'dddb-20171126'
+options.conddb_tag = 'sim-20171127-vc-md100'
+options.simulation = True
+options.output_file = outfolder+'PhiG.mdst'
+options.output_type = 'ROOT'
+options.output_level = INFO
+options.control_flow_file = outfolder+'control_flow.gv'
+options.data_flow_file = outfolder+'data_flow.gv'
