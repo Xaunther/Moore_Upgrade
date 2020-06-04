e626e462b689265fc45a42002fdf77ab0c2789a0
aalfonso <alejandro.alfonso.albero@cern.ch>
2020-06-04 09:03:51 +0200

    Output is dst actually

--- Moore_Scripts/K1G.py
+++ Moore_Scripts/K1G.py
@@ -2,14 +2,18 @@ from Moore import options
 from Gaudi.Configuration import INFO
 import os
 
+#Define decay name, will be used around
+decname = "K1GG"
+decdesc = "12203224"
+
 #Define outputfolder here
-outfolder = "output/K1G/"
+outfolder = "output/{0}/".format(decname)
 try:
     os.mkdir(outfolder)
 except:
     pass
 
-with open("DSTs/K1G_12203224_MagDown.dir") as f:
+with open("DSTs/{0}_{1}_MagDown.dir").format(decname, decdesc) as f:
     options.input_files = f.read().splitlines()
 f.close()
 options.input_type = 'ROOT'
@@ -20,7 +24,7 @@ options.data_type = 'Upgrade'
 options.dddb_tag = 'dddb-20171126'
 options.conddb_tag = 'sim-20171127-vc-md100'
 options.simulation = True
-options.output_file = outfolder+'K1G.ldst'
+options.output_file = outfolder+decname+'.dst'
 options.output_type = 'ROOT'
 options.output_level = INFO
 options.control_flow_file = outfolder+'control_flow.gv'
--- Moore_Scripts/KstG.py
+++ Moore_Scripts/KstG.py
@@ -2,14 +2,18 @@ from Moore import options
 from Gaudi.Configuration import INFO
 import os
 
+#Define decay name, will be used around
+decname = "KstG"
+decdesc = "11102202"
+
 #Define outputfolder here
-outfolder = "output/KstG/"
+outfolder = "output/{0}/".format(decname)
 try:
     os.mkdir(outfolder)
 except:
     pass
 
-with open("DSTs/KstG_11102202_MagDown.dir") as f:
+with open("DSTs/{0}_{1}_MagDown.dir").format(decname, decdesc) as f:
     options.input_files = f.read().splitlines()
 f.close()
 options.input_type = 'ROOT'
@@ -20,7 +24,7 @@ options.data_type = 'Upgrade'
 options.dddb_tag = 'dddb-20171126'
 options.conddb_tag = 'sim-20171127-vc-md100'
 options.simulation = True
-options.output_file = outfolder+'KstG.ldst'
+options.output_file = outfolder+decname+'.dst'
 options.output_type = 'ROOT'
 options.output_level = INFO
 options.control_flow_file = outfolder+'control_flow.gv'
