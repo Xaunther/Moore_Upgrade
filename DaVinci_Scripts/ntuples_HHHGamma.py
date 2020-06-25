8acc07a8c89d60d2925868fb268139e01844bb38
Xaunther <alex.1993.24@gmail.com>
2020-06-25 14:51:40 +0200

    Prepare DV extrasels

--- DaVinci_Scripts/ntuples_HHHGamma.py
+++ DaVinci_Scripts/ntuples_HHHGamma.py
@@ -21,7 +21,10 @@ extra_Ks0LL = "ExtraKs0LL"
 extra_Ks0DD = "ExtraKs0DD"
 extra_LambdaLL = "ExtraLambdaLL"
 extra_LambdaDD = "ExtraLambdaDD"
-dtt_list=[]
+extra_gamma = "ExtraGamma"
+extra_pi0_merged = "ExtraPi0Merged"
+extra_pi0_resolved = "ExtraPi0Resolved"
+dtt_list = []
 
 # The addTupleTool machinery tries to mutate this in place, so have to make
 # sure to copy it whenever we need it
@@ -62,11 +65,16 @@ dtt_extra_hadron.addTupleTool("TupleToolANNPID").ANNPIDTunes = ["MC15TuneV1"]
 dtt_list.append(dtt_extra_hadron)
 
 #Extra Ks0
-extra_Ks0LL_output = AutomaticData("{0}/{1}/Particles".format(linename, extra_Ks0LL))
-extra_Ks0DD_output = AutomaticData("{0}/{1}/Particles".format(linename, extra_Ks0DD))
+extra_Ks0LL_output = AutomaticData("{0}/{1}/Particles".format(
+    linename, extra_Ks0LL))
+extra_Ks0DD_output = AutomaticData("{0}/{1}/Particles".format(
+    linename, extra_Ks0DD))
 dtt_extra_Ks0 = DecayTreeTuple(
     "ExtraKs0",
-    Inputs=[extra_Ks0LL_output.outputLocation(), extra_Ks0DD_output.outputLocation()],
+    Inputs=[
+        extra_Ks0LL_output.outputLocation(),
+        extra_Ks0DD_output.outputLocation()
+    ],
     Decay="KS0 -> ^pi+ ^pi-",
     ToolList=list(DEFAULT_TUPLE_TOOLS),
 )
@@ -81,7 +89,10 @@ extra_LambdaDD_output = AutomaticData("{0}/{1}/Particles".format(
     linename, extra_LambdaDD))
 dtt_extra_Lambda = DecayTreeTuple(
     "ExtraLambda",
-    Inputs=[extra_LambdaLL_output.outputLocation(), extra_LambdaDD_output.outputLocation()],
+    Inputs=[
+        extra_LambdaLL_output.outputLocation(),
+        extra_LambdaDD_output.outputLocation()
+    ],
     Decay="(Lambda0 -> ^p+ ^pi-) || (Lambda~0 -> ^p~- ^pi+)",
     ToolList=list(DEFAULT_TUPLE_TOOLS),
 )
@@ -89,9 +100,40 @@ dtt_extra_Lambda.ErrorMax = -1
 dtt_extra_Lambda.addTupleTool("TupleToolANNPID").ANNPIDTunes = ["MC15TuneV1"]
 dtt_list.append(dtt_extra_Lambda)
 
+#Extra gamma
+extra_gamma_output = AutomaticData("{0}/{1}/Particles".format(
+    linename, extra_gamma))
+dtt_extra_gamma = DecayTreeTuple(
+    "ExtraGamma",
+    Inputs=[extra_gamma_output.outputLocation()],
+    Decay="gamma",
+    ToolList=list(DEFAULT_TUPLE_TOOLS),
+)
+dtt_extra_gamma.ErrorMax = -1
+dtt_extra_gamma.addTupleTool("TupleToolANNPID").ANNPIDTunes = ["MC15TuneV1"]
+dtt_list.append(dtt_extra_gamma)
+
+#Extra Pi0
+extra_Pi0Merged_output = AutomaticData("{0}/{1}/Particles".format(
+    linename, extra_pi0_merged))
+extra_Pi0Resolved_output = AutomaticData("{0}/{1}/Particles".format(
+    linename, extra_pi0_resolved))
+dtt_extra_Pi0 = DecayTreeTuple(
+    "ExtraPi0",
+    Inputs=[
+        extra_Pi0Merged_output.outputLocation(),
+        extra_Pi0Resolved_output.outputLocation()
+    ],
+    Decay="pi0",
+    ToolList=list(DEFAULT_TUPLE_TOOLS),
+)
+dtt_extra_Pi0.ErrorMax = -1
+dtt_extra_Pi0.addTupleTool("TupleToolANNPID").ANNPIDTunes = ["MC15TuneV1"]
+dtt_list.append(dtt_extra_Pi0)
+
 DaVinci().RootInTES = ROOT_IN_TES
 DaVinci().UserAlgorithms = dtt_list
-DaVinci().TupleFile = DaVinci().TupleFile + "_"+output+".root"
+DaVinci().TupleFile = DaVinci().TupleFile + "_" + output + ".root"
 
 
 @appendPostConfigAction
