edb71ba5850b7ec2cecad5730a151afeb4a978c3
Alejandro Alfonso <aalfonso@cern.ch>
2021-03-26 17:25:43 +0100

    Remove Lumi option. Not present in LHCbApp

--- DaVinci_Scripts/KstG.py
+++ DaVinci_Scripts/KstG.py
@@ -9,4 +9,4 @@ LHCbApp().DDDBtag = 'dddb-20171126'
 LHCbApp().CondDBtag = 'sim-20171127-vc-md100'
 LHCbApp().DataType = "Upgrade"
 LHCbApp().Simulation = True
-LHCbApp().Lumi = not LHCbApp().Simulation
+#LHCbApp().Lumi = not LHCbApp().Simulation
